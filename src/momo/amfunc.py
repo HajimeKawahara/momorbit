6ca74ce20267458ed563f578b70fa1a19d806bfa
Hajime Kawahara <divrot@gmail.com>
2020-11-18 09:16:16 +0900

    XYZ

--- src/momo/amfunc.py
+++ src/momo/amfunc.py
@@ -20,6 +20,7 @@ def XYZf(t,T0,P,e,omegaA,OmegaL,a,i):
     sinf[mask]=-sinf[mask]
     cosoA=np.cos(omegaA)
     sinoA=np.sin(omegaA)
+    
     cosOL=np.cos(OmegaL)
     sinOL=np.sin(OmegaL)
     
@@ -35,17 +36,43 @@ def XYZf(t,T0,P,e,omegaA,OmegaL,a,i):
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    t=np.linspace(0,1.0,100)
+    t=np.linspace(0,100.0,100)
     T0=0
-    P=0.25
-    e=0.3
-    omegaA=np.pi
-    OmegaL=np.pi
-    a=1.0
-    i=np.pi/3.0
 
+    par=99.20 #parallax mas
+    asec= 0.51026 #a [arcsec]
+    a = asec/(par*1.e-3)        #[A.U.]
+    e = 0.1576
+    i = 44.29       #[deg]
+    
+    node = 176.75    #[deg]
+    
+    w = 104.90      #[deg]
+    
+    tp=57447.0 + 2400000.5
+    P = 15.4275 #[yr]
+
+    #
+    par=18.9878 #parallax mas
+    asec= 0.271 #a [arcsec]
+    a = asec/(par*1.e-3)        #[A.U.]
+    e = 0.89
+    i = 133.5       #[deg]
+    node = 214.8    #[deg]
+    w = 110.3      #[deg]
+
+    i=i/180*np.pi
+    omegaA = w*np.pi/180.0      #[rad]
+    OmegaL = node*np.pi/180.0    #[rad]
+
+    
     X,Y,Z=XYZf(t,T0,P,e,omegaA,OmegaL,a,i)
     
-    plt.plot(X,Y,".")
+    fig=plt.figure()
+    ax=fig.add_subplot(111,aspect=1.0)
+    ax.plot(-Y,X,".")
+    ax.plot([0],[0],"+")
+    plt.xlim(-a*(1+e),a*(1+e))
+    plt.ylim(-a*(1+e),a*(1+e))
     plt.show()
 
